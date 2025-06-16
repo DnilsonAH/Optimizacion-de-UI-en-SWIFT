7cadb6db9421c60d749d9e325c924bbe8f7a99de
DnilsonAH <dnilson.achahuanco@tecsup.edu.pe>
2025-06-16 16:34:08 -0500

    Initial Commit

---
+++ ProyectoAnimaciones/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ProyectoAnimaciones
+//
+//  Created by Dnilson Achahuanco on 16/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProyectoAnimaciones/ProyectoAnimacionesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoAnimacionesApp.swift
+//  ProyectoAnimaciones
+//
+//  Created by Dnilson Achahuanco on 16/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoAnimacionesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
